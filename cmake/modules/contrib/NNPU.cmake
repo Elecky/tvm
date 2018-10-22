6498bdffd874981b70733a3bdbb911936f3c4061
jian.hu <elecky@outlook.com>
2018-10-22 14:01:33 +0800

    modified folder structure.

--- cmake/modules/contrib/NNPU.cmake
+++ cmake/modules/contrib/NNPU.cmake
@@ -6,10 +6,10 @@ if(PYTHON)
     set(NNPU_RUNTIME_SRCS ${RUNTIME_SRC}/device_api.cpp ${RUNTIME_SRC}/runtime.cpp 
                           ${RUNTIME_SRC}/sim_driver.cpp ${RUNTIME_SRC}/insn.cpp)
     set(SIM_SRC nnpu/NNPUSim/src/)
-    set(NNPU_S0SIM_SRC ${SIM_SRC}/S0Simulator.cpp ${SIM_SRC}/bit_packer.cpp
-                       ${SIM_SRC}/bit_packer_factory.cpp ${SIM_SRC}/bit_wrapper.cpp
-                       ${SIM_SRC}/ram.cpp)
-    add_library(nnpu SHARED ${NNPU_RUNTIME_SRCS} ${NNPU_S0SIM_SRC})
+    set(NNPU_SIM_COMMON ${SIM_SRC}/common/bit_packer.cpp ${SIM_SRC}/common/bit_packer_factory.cpp
+                        ${SIM_SRC}/common/bit_wrapper.cpp ${SIM_SRC}/common/wire.cpp)
+    set(NNPU_S0SIM_SRC ${SIM_SRC}/ram.cpp ${SIM_SRC}/S0Simulator.cpp)
+    add_library(nnpu SHARED ${NNPU_RUNTIME_SRCS} ${NNPU_SIM_COMMON} ${NNPU_S0SIM_SRC})
     target_include_directories(nnpu PUBLIC nnpu/include /usr/local/include nnpu/NNPUSim/include)
     target_link_libraries(nnpu -lyaml-cpp)
 else()
