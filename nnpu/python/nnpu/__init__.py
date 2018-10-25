2d78c00e06f710296ff093ea374075dcb4ab0701
jian.hu <elecky@outlook.com>
2018-10-25 12:20:28 +0800

    modified schedule helper, now you can use context. added reshape and transpose wrapper.

--- nnpu/python/nnpu/__init__.py
+++ nnpu/python/nnpu/__init__.py
@@ -2,6 +2,7 @@ from .libinfo import _load_lib
 from .environment import Environment, get_env, set_device
 from .build_module import lower, build
 from .utils import *
+from .utils import create_schedule
 
 # load nnpu library
 _load_lib()
--- nnpu/python/nnpu/utils.py
+++ nnpu/python/nnpu/utils.py
@@ -1,11 +1,14 @@
 import tvm
 from .environment import get_env
 from .helper import convert_scope
+import topi
 
 class ScheduleProcHelper(object):
     '''
     a helper method to collect the schedule transforming closures.
     '''
+    current = None
+
     def __init__(self):
         self.closures = []
         self.env = get_env()
@@ -18,12 +21,26 @@ class ScheduleProcHelper(object):
     def Transform(self, sc):
         for f in self.closures:
             f(sc)
+        self.closures = []
     
     def MarkScope(self, tensor, scope='uni'):
         scope = convert_scope(self.env, scope)
         self.Add(lambda sc: sc[tensor].set_scope(scope))
+    
+    def __enter__(self):
+        self.last = ScheduleProcHelper.current
+        ScheduleProcHelper.current = self
+        return self
+    
+    def __exit__(self, ptype, value, trace):
+        ScheduleProcHelper.current = self.last
 
-def DMACopyHtoDram(tensor, name_prefix, sph):
+def MarkScope(tensor, sph=None, scope='uni'):
+    ScheduleProcHelper.current.MarkScope(tensor, scope)
+
+def DMACopyHtoDram(tensor, name_prefix, sph=None):
+    sph = ScheduleProcHelper.current if sph is None else sph
+    
     env = get_env()
     tensor_dram = tvm.compute(tensor.shape, lambda *i: tensor(*i), name_prefix + "_dram")
     
@@ -32,7 +49,9 @@ def DMACopyHtoDram(tensor, name_prefix, sph):
     
     return tensor_dram
 
-def CopyHtoBuf(tensor, name_prefix, sph, dst_scope='uni'):
+def CopyHtoBuf(tensor, name_prefix, sph=None, dst_scope='uni'):
+    sph = ScheduleProcHelper.current if sph is None else sph
+
     env = get_env()
     tensor_dram = DMACopyHtoDram(tensor, name_prefix, sph)
     tensor_buf = tvm.compute(tensor.shape, lambda *i: tensor_dram(*i), name_prefix + "_buf")
@@ -43,7 +62,9 @@ def CopyHtoBuf(tensor, name_prefix, sph, dst_scope='uni'):
 
     return tensor_buf, tensor_dram
 
-def CopyBufToDram(tensor, name_prefix, sph):
+def CopyBufToDram(tensor, name_prefix, sph=None):
+    sph = ScheduleProcHelper.current if sph is None else sph
+
     env = get_env()
     tensor_dram = tvm.compute(tensor.shape, lambda *i: tensor(*i), name_prefix + "_dram")
     
@@ -52,7 +73,9 @@ def CopyBufToDram(tensor, name_prefix, sph):
     
     return tensor_dram
 
-def CopyBufToH(tensor, name_prefix, sph):
+def CopyBufToH(tensor, name_prefix, sph=None):
+    sph = ScheduleProcHelper.current if sph is None else sph
+
     env = get_env()
     tensor_dram = CopyBufToDram(tensor, name_prefix, sph)
     tensor_host = tvm.compute(tensor_dram.shape, lambda *i: tensor_dram(*i), name_prefix + '_host')
@@ -60,3 +83,29 @@ def CopyBufToH(tensor, name_prefix, sph):
     sph.Add(lambda sc: sc[tensor_host].pragma(sc[tensor_host].op.axis[0], env.dma_copy_pragma))
 
     return tensor_host, tensor_dram
+
+def PragmaCopy(tensor, sph=None):
+    env = get_env()
+    sph = ScheduleProcHelper.current if sph is None else sph
+    sph.Add(lambda sc: sc[tensor].pragma(tensor.op.axis[0], env.scratchpad_copy))
+
+def reshape(tensor, shape, sph=None, dst_scope='uni'):
+    res = topi.reshape(tensor, shape)
+    
+    MarkScope(res, dst_scope)
+    PragmaCopy(res)
+
+    return res
+
+def transpose(tensor, axes=None, sph=None, dst_scope='uni'):
+    res = topi.transpose(tensor, axes)
+
+    MarkScope(res, dst_scope)
+    PragmaCopy(res)
+    return res
+
+
+def create_schedule(*args, **kwargs):
+    s = tvm.create_schedule(*args, **kwargs)
+    ScheduleProcHelper.current.Transform(s)
+    return s
