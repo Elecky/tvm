16bfcc395f9892f9a9cf3cacc1dc451863162f63
xlzhang223 <xlzhang223@163.com>
2018-10-27 17:17:49 +0800

    pooling max and mean

---
+++ nnpu/tests/test_poolingmax.py
@@ -0,0 +1,85 @@
+import nnpu
+import tvm
+import topi
+from nnpu.utils import ScheduleProcHelper
+import numpy as np
+
+
+def max_pooling(inshape,outshape,cell_shape,innp,outdetype):
+  ret=np.zeros(outshape, dtype=outdetype)
+  for w in range(outshape[0]):
+    for h in range(outshape[1]):
+      for j in range(cell_shape):
+        for k in range(cell_shape):
+          for l in range(outshape[2]):
+            ret[w][h][l]=max(ret[w][h][l],innp[w*4+j][h*4+k][l])
+  return ret    
+
+
+# reduce max
+def test():
+    env = nnpu.get_env()
+    nnpu.set_device(env)
+    in_shape = (16,16,32)
+    cell_shape = 4
+    out_shape = (4,4,32)
+    reduce_shap=(0,cell_shape)
+    dtype_n, dtype_w = env.cfg['dtype_n'], env.cfg['dtype_w']
+    
+    sph = ScheduleProcHelper()
+    str_op = 'VGTMMerge'
+    a = tvm.placeholder(in_shape, dtype_w, 'a')
+    a_buf, _1 = nnpu.utils.CopyHtoBuf(a, 'a', sph)
+
+    k1 = tvm.reduce_axis(reduce_shap, 'k1')
+    step1_buf = tvm.compute((in_shape[0],in_shape[1]//cell_shape,in_shape[2]), 
+                        lambda i,j,k: 
+                         tvm.sum(a_buf[i,j*4+k1,k],axis=k1),
+                       'step1_buf')
+    sph.MarkScope(step1_buf)
+
+    k1 = tvm.reduce_axis(reduce_shap, 'k2')
+    step2_buf = tvm.compute(out_shape, 
+                        lambda i,j,k: 
+                         tvm.sum(step1_buf[i*4+k1,j,k],axis=k1),
+                       'step2_buf')
+    sph.MarkScope(step2_buf)
+    
+    step2_host, step2_dram = nnpu.utils.CopyBufToH(step2_buf, 'step2',sph)
+    s = tvm.create_schedule(step2_host.op)
+    sph.Transform(s)
+
+
+    #tensorize
+    ko, ki = s[step1_buf].split(step1_buf.op.reduce_axis[0], factor=1)
+    xo,xi = s[step1_buf].split(step1_buf.op.axis[2], factor=16)
+    s[step1_buf].reorder( step1_buf.op.axis[0],step1_buf.op.axis[1],xo,ko,ki,xi)
+    s[step1_buf].tensorize(ki, env.intrins.get(str_op,  mode='w'))
+
+    ko1, ki1 = s[step2_buf].split(step2_buf.op.reduce_axis[0], factor=1)
+    xo1,xi1 = s[step2_buf].split(step2_buf.op.axis[2], factor=16)
+    s[step2_buf].reorder( step2_buf.op.axis[0],step2_buf.op.axis[1],xo1,ko1,ki1, xi1)
+    s[step2_buf].tensorize(ki1, env.intrins.get(str_op,  mode='w',nDim=3))
+    
+    print(nnpu.lower(s, [a, step2_host], simple_mode=True))
+   # exit()
+    func = nnpu.build(s, [a, step2_host], 'nnpu', 'llvm', name='nnpu_func')
+
+    ctx = tvm.nd.TVMContext(13, 0)
+    a_np = np.random.randint(size=in_shape, dtype=a.dtype, low = 0, high = 8)
+    a_nd = tvm.nd.array(a_np, ctx)
+
+    c_nd = tvm.nd.array(np.zeros(out_shape, dtype=step2_host.dtype), ctx)
+
+    func(a_nd, c_nd)
+    print("pooling-max")
+    print(c_nd.asnumpy())
+    
+    print("nppooling-max")
+    gt=max_pooling(in_shape,out_shape,cell_shape,a_np,a.dtype)
+    print(gt)
+    np.testing.assert_allclose(c_nd.asnumpy(), gt)
+    
+
+if __name__ == '__main__':
+    test()
--- nnpu/tests/test_poolingmean.py
+++ nnpu/tests/test_poolingmean.py
@@ -12,10 +12,12 @@ def mean_pooling(inshape,outshape,cell_shape,innp,outdetype):
       for j in range(cell_shape):
         for k in range(cell_shape):
           ret[w][h]=ret[w][h]+innp[w*4+j][h*4+k]
+
   di=cell_shape*cell_shape
   for w in range(outshape[0]):
     for h in range(outshape[1]):
       ret[w][h]=ret[w][h]/di
+
   return ret    
 
 
@@ -26,6 +28,7 @@ def test():
     in_shape = (16,16,32)
     cell_shape = 4
     out_shape = (4,4,32)
+    reduce_shap=(0,cell_shape)
     dtype_n, dtype_w = env.cfg['dtype_n'], env.cfg['dtype_w']
     
     sph = ScheduleProcHelper()
@@ -33,29 +36,30 @@ def test():
     a = tvm.placeholder(in_shape, dtype_w, 'a')
     a_buf, _1 = nnpu.utils.CopyHtoBuf(a, 'a', sph)
 
-    k1 = tvm.reduce_axis((0, 4), 'k1')
-    step1_buf = tvm.compute((4,16,32), 
-                        lambda j,i,k: 
+    k1 = tvm.reduce_axis(reduce_shap, 'k1')
+    step1_buf = tvm.compute((in_shape[0],in_shape[1]//cell_shape,in_shape[2]), 
+                        lambda i,j,k: 
                          tvm.sum(a_buf[i,j*4+k1,k],axis=k1),
                        'step1_buf')
     sph.MarkScope(step1_buf)
 
-    k1 = tvm.reduce_axis((0, 4), 'k2')
-    step2_buf = tvm.compute((4,4,32), 
+    k1 = tvm.reduce_axis(reduce_shap, 'k2')
+    step2_buf = tvm.compute(out_shape, 
                         lambda i,j,k: 
-                         tvm.sum(step1_buf[j,i*4+k1,k],axis=k1),
+                         tvm.sum(step1_buf[i*4+k1,j,k],axis=k1),
                        'step2_buf')
     sph.MarkScope(step2_buf)
     
-
+    
     Imm = tvm.const(cell_shape*cell_shape, env.cfg['dtype_w'])
-    step3_buf = tvm.compute((4,4,32), 
+    step3_buf = tvm.compute(out_shape, 
                         lambda i,j,k: 
-                         step2_buf[i,j,k]/Imm,
-                       'step3_buf')
+                        step2_buf[i,j,k]/Imm,
+                      'step3_buf')
     sph.MarkScope(step3_buf)
-
+    
     step3_host, step3_dram = nnpu.utils.CopyBufToH(step3_buf, 'step3',sph)
+    #step2_host, step2_dram = nnpu.utils.CopyBufToH(step2_buf, 'step3',sph)
     s = tvm.create_schedule(step3_host.op)
     sph.Transform(s)
 
@@ -63,37 +67,39 @@ def test():
     #tensorize
     ko, ki = s[step1_buf].split(step1_buf.op.reduce_axis[0], factor=1)
     xo,xi = s[step1_buf].split(step1_buf.op.axis[2], factor=16)
-    s[step1_buf].reorder( step1_buf.op.axis[1],step1_buf.op.axis[0],xo,ko,ki,xi)
+    s[step1_buf].reorder( step1_buf.op.axis[0],step1_buf.op.axis[1],xo,ko,ki,xi)
     s[step1_buf].tensorize(ki, env.intrins.get(str_op,  mode='w'))
 
     ko1, ki1 = s[step2_buf].split(step2_buf.op.reduce_axis[0], factor=1)
     xo1,xi1 = s[step2_buf].split(step2_buf.op.axis[2], factor=16)
     s[step2_buf].reorder( step2_buf.op.axis[0],step2_buf.op.axis[1],xo1,ko1,ki1, xi1)
-    s[step2_buf].tensorize(ki1, env.intrins.get(str_op,  mode='w'))
-
+    s[step2_buf].tensorize(ki1, env.intrins.get(str_op,  mode='w',nDim=3))
+    
     xo2,xi2 = s[step3_buf].split(step3_buf.op.axis[2], factor=16)
     s[step3_buf].reorder( step3_buf.op.axis[0],step3_buf.op.axis[1],xo2,xi2)
-    s[step3_buf].tensorize(xi2, env.intrins.get('VDivI',  mode='w'))
-
+    s[step3_buf].tensorize(xi2, env.intrins.get('VDivI',imm_value=Imm.value,  mode='w'))
+    
     
     
 
     print(nnpu.lower(s, [a, step3_host], simple_mode=True))
-    exit()
+   # exit()
     func = nnpu.build(s, [a, step3_host], 'nnpu', 'llvm', name='nnpu_func')
 
     ctx = tvm.nd.TVMContext(13, 0)
-    a_np = np.random.randint(size=in_shape, dtype=a.dtype, low = -8, high = 8)
+    a_np = np.random.randint(size=in_shape, dtype=a.dtype, low = 0, high = 8)
     a_nd = tvm.nd.array(a_np, ctx)
 
-    c_nd = tvm.nd.array(np.zeros(out_shape, dtype=step2_host.dtype), ctx)
+    c_nd = tvm.nd.array(np.zeros(out_shape, dtype=step3_host.dtype), ctx)
 
     func(a_nd, c_nd)
-    print("pooling-sum")
+    print("pooling-mean")
     print(c_nd.asnumpy())
     
-    print("nppooling-sum")
-    print(mean_pooling(in_shape,out_shape,cell_shape,a_np,a.dtype))
+    print("nppooling-mean")
+    gt=mean_pooling(in_shape,out_shape,cell_shape,a_np,a.dtype)
+    print(gt)
+    np.testing.assert_allclose(c_nd.asnumpy(), gt)
     
 
 if __name__ == '__main__':
