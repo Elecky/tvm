aed77b1a85ffa2cacdb1f80f2265e83d7c9101c1
jian.hu <elecky@outlook.com>
2018-11-04 16:27:41 +0800

    finished some cycle.

--- cmake/modules/contrib/NNPU.cmake
+++ cmake/modules/contrib/NNPU.cmake
@@ -7,10 +7,12 @@ if(PYTHON)
                           ${RUNTIME_SRC}/sim_driver.cpp ${RUNTIME_SRC}/insn.cpp)
     set(SIM_SRC nnpu/NNPUSim/src/)
     set(NNPU_SIM_COMMON ${SIM_SRC}/common/bit_packer.cpp ${SIM_SRC}/common/bit_packer_factory.cpp
-                        ${SIM_SRC}/common/bit_wrapper.cpp ${SIM_SRC}/common/wire.cpp)
+                        ${SIM_SRC}/common/bit_wrapper.cpp ${SIM_SRC}/common/wire.cpp
+                        )
     set(NNPU_S0SIM_SRC ${SIM_SRC}/ram.cpp ${SIM_SRC}/S0Simulator.cpp)
     set(NNPU_S1SIM_SRC ${SIM_SRC}/sim_module.cpp ${SIM_SRC}/insn_mem.cpp
-                       ${SIM_SRC}/insn_wrapper.cpp ${SIM_SRC}/insn_decoder.cpp)
+                       ${SIM_SRC}/insn_wrapper.cpp ${SIM_SRC}/insn_decoder.cpp
+                       ${SIM_SRC}/controller.cpp ${SIM_SRC}/reservation_station.cpp)
     add_library(nnpu SHARED ${NNPU_RUNTIME_SRCS} ${NNPU_SIM_COMMON} ${NNPU_S0SIM_SRC}
                             ${NNPU_S1SIM_SRC})
     target_include_directories(nnpu PUBLIC nnpu/include /usr/local/include nnpu/NNPUSim/include)
--- nnpu/tests/cpp/test_main.cpp
+++ nnpu/tests/cpp/test_main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <nnpusim/insn_mem.h>
 #include <nnpusim/insn_decoder.h>
+#include <nnpusim/controller.h>
+#include <nnpusim/reg_file_module.h>
 #include <vector>
 
 using namespace nnpu;
@@ -15,40 +17,60 @@ std::vector<NNPUInsn> init_insn()
     insns.emplace_back(Li(1, 0));
     insns.emplace_back(Li(2, 233));
     insns.emplace_back(Li(31, 0xff));
-    insns.emplace_back(nnpu::JumpInsn(0));
+    insns.emplace_back(nnpu::JumpInsn(-4));
 
     return insns;
+}
+
+WireData<bool> branchOut(int *i, int on)
+{
+    if (*i == on)
+    {
+        return WireData<bool>(true, true);
+    }
+    else
+    {
+        return WireData<bool>(false);
+    }
 }
 
 int main(int argc, char *(argv[]))
 {
     WireManager wm;
+    vector<shared_ptr<SimModule>> modules;
     YAML::Node cfg = YAML::LoadFile("/home/jian/repositories/tvm/nnpu/nnpu_config.yaml");
     std::shared_ptr<InsnMemModule> IF(new InsnMemModule(wm, cfg));
+    modules.push_back(IF);
     //cout << IF.get() << endl;
     IF->SetInsns(init_insn());
     IF->BindWires(wm);
 
     std::shared_ptr<InsnDecoder> ID(new InsnDecoder(wm, cfg));
+    modules.push_back(ID);
     ID->BindWires(wm);
 
-    auto IFOut = wm.Get<InsnWrapper>("ID_out");
-    InsnDumper dumper;
-    for (int i = 0; i < 10; ++i)
-    {
-        IF->Move();
-        ID->Move();
-        auto ifOut = IFOut->Read();
+    std::shared_ptr<Ctrl> ctrl(new Ctrl(wm, cfg));
+    modules.push_back(ctrl);
+    ctrl->BindWires(wm);
 
-        if (ifOut.HasData)
+    std::shared_ptr<RegFileMod> regs(new RegFileMod(wm, cfg));
+    modules.push_back(std::static_pointer_cast<SimModule>(regs));
+    
+    int i;
+    wm.Get<bool>("branch_out")->SubscribeWriter(std::bind(branchOut, &i, 12));
+    for (i = 0; i < 20; ++i)
+    {
+        for (auto m : modules)
         {
-            cout << "# " << ifOut.Data.pc << ": ";
-            ifOut.Data.insn->Call(dumper, cout);
-            cout << endl;
+            m->Move();
         }
 
-        IF->Update();
-        ID->Update();
+        cout << "start of cycle :" << i + 1 << endl;
+
+        for (auto m : modules)
+        {
+            m->Update();
+        }
     }
 
     return 0;
