febe1de3e468857ae5317bb37dc9c69f56728544
xlzhang223 <xlzhang223@163.com>
2019-02-22 14:45:40 +0800

    pull

--- nnpu/tests/test_nms.py
+++ nnpu/tests/test_nms.py
@@ -10,54 +10,70 @@ def test():
     print('aaaa')
     env = nnpu.get_env()
     nnpu.set_device(env)
-    shape = (32,)
+    shape = (64,5)
     dtype_n, dtype_w = env.cfg['dtype_n'], env.cfg['dtype_w']
     a = tvm.placeholder(shape, dtype_w, name='a')
+    w = shape[0]
+    e = 16
     def build_nms_ir(ten_in,ten_out):
         env = nnpu.get_env()
         ib = tvm.ir_builder.create()
         ib.scope_attr(env.nnpu_axis, "coproc_scope", 0)
         p_in = ib.buffer_ptr(ten_in[0])
         p_out = ib.buffer_ptr(ten_out[0])
-        '''
-        ib.emit(tvm.call_extern("int32", 'NNPU_Memset',
-                                ten_out[0].access_ptr('w', 'uint32'), 32, 2,
-                                str(0), 3
-                    ))
-        '''
-        five = tvm.const(5, env.cfg['dtype_w'])
-        with ib.for_range(0, 32, name="i") as i:
-            p_out[i] = p_in[i]
-            with ib.if_scope((i % 2) == 0):
-                p_out[i] = p_in[i] + five
+        with ib.for_range(0,w, name="k") as k:
+            with ib.for_range(0,w/e, name="i") as i:
+                ib.emit(tvm.call_extern("int32", 'NNPU_Reducebykey', ))
+            ib.emit(tvm.call_extern("int32", 'NNPU_cal_S', ))
+            with ib.for_range(0,w/e, name="j") as j:
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_max-1',))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_min-3', ))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_min3-max1',))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_GTM0', ))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_max-2', ))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_min-4', ))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_min4-max2', ))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_GTM0', ))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_(min3-max1)*(min4-max2)', ))
+                ib.emit(tvm.call_extern("int32", 'NNPU_cal_iou',))
+                ib.emit(tvm.call_extern("int32", 'NNPU_comp_and_set', ))
         stmt = ib.get()
         return stmt
-    #sph = ScheduleProcHelper()
-    #a_buf, a_dram = nnpu.utils.CopyHtoBuf(a, 'a', sph)
-    #sph.MarkScope(a_buf)
-    b_buf = tvm.extern([(32,)],[a], build_nms_ir ,dtype=dtype_w,tag="test_ir")
-    #sph.MarkScope(b_buf)
-    #b_host, b_dram = nnpu.utils.CopyBufToH(b_buf, 'b', sph)
-    s = tvm.create_schedule([b_buf.op])
-    #sph.Transform(s)
-    print(nnpu.lower(s, [a ,b_buf], simple_mode=True))
+    sph = ScheduleProcHelper()
+    a_buf, a_dram = nnpu.utils.CopyHtoBuf(a, 'a', sph)
+    sph.MarkScope(a_buf)
+    out = tvm.extern(a_buf.shape,[a_buf],
+                    build_nms_ir,
+                    in_buffers=[tvm.decl_buffer(a_buf.shape,dtype_w,
+                                    data_alignment=8,
+                                    scope = 'local.nnpu_scratchpad')]
+                    ,out_buffers=[tvm.decl_buffer(a_buf.shape,dtype_w,
+                                    data_alignment=8,
+                                    scope = 'local.nnpu_scratchpad')]
+                    ,dtype=dtype_w,name="test_ir")
+    sph.MarkScope(out)
+    out_host, out_dram = nnpu.utils.CopyBufToH(out, 'out', sph)
+    s = tvm.create_schedule([out_host.op])
+    sph.Transform(s)
+    print(nnpu.lower(s, [a,out_host], simple_mode=True))
     exit(0)
-    func = nnpu.build(s, [a ,b_host], 'nnpu', 'llvm', name='nnpu_reduce')
+    func = nnpu.build(s, [a,out_host], 'nnpu', 'llvm', name='nnpu_reduce')
     ctx = tvm.nd.TVMContext(13, 0)
-    a_np = np.random.randint(size=(5,16), dtype=a.dtype, low = 0, high = 127)
+    a_np = np.random.randint(size=(32,), dtype=a.dtype, low = 0, high = 127)
     a_nd = tvm.nd.array(a_np, ctx)
     
-    b_nd = tvm.nd.array(np.zeros((5,)).astype(b_host.dtype), ctx)
+    b_nd = tvm.nd.array(np.zeros(32,).astype(out_host.dtype), ctx)
 
     func(a_nd, b_nd)
 
     print('a = ')
     print(a_np)
-    print('reduce sum row = ')
+    print('xjb sum = ')
     print(b_nd.asnumpy())
 
     return
-
+def test_ib():
+    return
 '''
 def test():
     print('aaaa')
