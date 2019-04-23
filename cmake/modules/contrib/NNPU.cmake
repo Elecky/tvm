b7e9387e3e773c08823e983ab3cce7c7a39a0273
jian.hu <dalejones@live.cn>
2019-04-23 16:29:47 +0800

    refactored NNPUSim.

--- cmake/modules/contrib/NNPU.cmake
+++ cmake/modules/contrib/NNPU.cmake
@@ -18,10 +18,24 @@ if(PYTHON)
 
   set(NNPU_S0SIM_SRC ${SIM_SRC}/ram.cpp ${SIM_SRC}/S0Simulator.cpp)
   set(NNPU_S1SIM_SRC ${SIM_SRC}/insn_wrapper.cpp 
-                     ${SIM_SRC}/address_range.cpp
+                     ${SIM_SRC}/address_range.cpp ${SIM_SRC}/insn_opcode.cpp
                      )
   
   set(NNPU_SCSIM_DIR ${SIM_SRC}/sc_sim)
+  set(NNPU_SCSIM_EXEC_DIR ${NNPU_SCSIM_DIR}/instructions/)
+  set(NNPU_SCSIM_EXEC_SRCS ${NNPU_SCSIM_EXEC_DIR}/gemm.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/mat_binary.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/mat_imm.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/mat_reduce_row.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/mat_row_dot.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/mat_vctr.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/vctr_binary.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/vctr_unary.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/vctr_imm.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/vctr_reduce.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/vctr_sclr.cpp
+                           ${NNPU_SCSIM_EXEC_DIR}/vctr_dot_prod.cpp
+                           )
   set(NNPU_SCSIM_SRCS ${NNPU_SCSIM_DIR}/sc_simulator.cpp ${NNPU_SCSIM_DIR}/ifetch.cpp
                       ${NNPU_SCSIM_DIR}/idecode.cpp ${NNPU_SCSIM_DIR}/issue_queue.cpp
                       ${NNPU_SCSIM_DIR}/future_file.cpp ${NNPU_SCSIM_DIR}/reserve_station.cpp
@@ -34,7 +48,8 @@ if(PYTHON)
                       ${NNPU_SCSIM_DIR}/dram_access_unit.cpp ${NNPU_SCSIM_DIR}/mat_calc_unit.cpp
                       ${NNPU_SCSIM_DIR}/matrix_unit.cpp ${NNPU_SCSIM_DIR}/ram_access.cpp
                       ${NNPU_SCSIM_DIR}/ram_channel.cpp ${NNPU_SCSIM_DIR}/buffer_access_spliter.cpp
-                      ${NNPU_SCSIM_DIR}/mem_copy_set_unit.cpp)
+                      ${NNPU_SCSIM_DIR}/mem_copy_set_unit.cpp
+                      ${NNPU_SCSIM_EXEC_SRCS})
 
   add_library(nnpu-dummy SHARED ${NNPU_SCSIM_DIR}/dummy.cpp)
   set_target_properties(nnpu-dummy PROPERTIES CXX_VISIBILITY_PRESET default)
